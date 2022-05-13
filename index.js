16fc1352fd32ca7a20278696eb972ccbe7d3d1f8
44ertenka <tetrub@gmail.com>
2022-05-13 13:58:05 +0300

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+let burger = document.getElementById('burger')
+let scroll = document.getElementById('scroll-off')
+let menu = document.getElementById('slide-menu')
+
+burger.addEventListener('click', ()=> {
+	burger.classList.toggle('active')
+	scroll.classList.toggle('active')
+	menu.classList.toggle('active')
+})
+
+console.log(thshyj)
